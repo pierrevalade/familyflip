69ce88a235bb51384acc81cc3809b6dfe04f7582
Pierre Valade <pierre@valade.info>
2010-05-10 11:47:41 -0700

    debuging

--- app/models/message.rb
+++ app/models/message.rb
@@ -30,7 +30,7 @@ class Message < ActiveRecord::Base
     self.subject = email.subject
     self.text = email.text_body
     email.attachments.each do |file|
-      self.images.build(:attachment => open(file))
+      self.images.build(:attachment => file)
     end
   end
   
