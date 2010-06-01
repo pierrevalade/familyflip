5df230043fa696f875c12496aa617726c5c8a697
Pierre Valade <pierre@valade.info>
2010-05-31 17:05:32 -0700

    youtube videos fixed

--- config/environment.rb
+++ config/environment.rb
@@ -25,6 +25,7 @@ Rails::Initializer.run do |config|
   config.gem "oembed_links"
   config.gem "rubyzip", :lib => "zip/zipfilesystem"
   # config.gem "httparty"
+  config.gem "uuidtools"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
